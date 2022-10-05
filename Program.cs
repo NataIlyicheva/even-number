43b691fa296b02ce3d5954bf4376e2d2084e9092
Natali <n7483205@ya.ru>
2022-10-05 21:51:35 +0700

    even number

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число.");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 2 ==0)
+
+{
+    Console.WriteLine($"{num} - четное число.");
+}
+else
+{
+    Console.WriteLine($"{num} - нечетное число.");
+}
